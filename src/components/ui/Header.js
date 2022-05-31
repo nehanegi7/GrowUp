15daf90133b3012f2d8b0d45c0d67552de06b475
nehanegi <nehanegi2710@gmail.com>
2022-05-31 23:12:46 +0530

    add appbar component-video12

---
+++ src/components/ui/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+const Header = () => {
+    return (
+        <>
+            <AppBar position='fixed'>
+                <Toolbar>
+                    Grow Up
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Header
